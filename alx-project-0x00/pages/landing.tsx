c9ee466f6625d086cd0f324926de5a6e28201a83
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-15 09:34:21 +0300

    added Card to Landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Card from '@/components/Card'
 
 function landing() {
     return (
         <div>
             <h1 className=" text-xl font-extralight">Landing Page</h1>
-
+            <Card />
         </div>
     )
 }
