99933fec83c929283f9246fc31ff78f409f10a96
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-15 09:08:53 +0300

    added about.tsx & landing.tsx pages

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+    return (
+        <div>
+            <h1 className="text-4xl font-semibold text-center mt-10">About Us</h1>
+        </div>
+    )
+}
+
+export default about
